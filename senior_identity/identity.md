ad0831be9b7aefe61e6d2e177df515ac80e4f523
KaiChen <237745537@qq.com>
2018-04-17 16:56:53 +0800

    特性finished

--- senior_identity/identity.md
+++ senior_identity/identity.md
@@ -1,1 +1,29 @@
 高级特性
+
+#### 1.切片
+L = ['dog', 'cat', 'lion', 'donkey', 'duck']
+取前3个元素
+```
+L[0:3]
+L[:3]
+['dog', 'cat', 'lion']
+```
+取倒数第一个元素（倒数第一个元素索引为-1）
+```
+L[-1:]
+['duck']
+```
+
+#### 2.迭代
+```
+d = {'cat':'10','lion':200,'duck':50}
+for key in d:
+    print(key)
+    print(d[key])
+cat
+10
+lion
+200
+duck
+50
+```
--- senior_identity/identity1.py
+++ senior_identity/identity1.py
@@ -1,1 +1,29 @@
 #coding=utf-8
+
+def add_list():
+    L = []
+    n = 1
+    while n < 99:
+        L.append(n)
+        n = n+2
+    return L
+list = add_list()
+print(list)
+
+L = ['dog', 'cat', 'lion', 'donkey', 'duck']
+print(L[0:3])
+print(L[:3])
+print(L[-1:])
+
+
+d = {'cat':'10','lion':200,'duck':50}
+for key in d:
+    print(key)
+    print(d[key])
+
+g = (x * x for x in range(10))
+print(g.__next__())
+print(g.__next__())
+print(g.__next__())
+print(g.__next__())
+print(g.__next__())
